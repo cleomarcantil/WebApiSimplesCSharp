163ae810e04c6dbeedbf3c207a02de25a25e37b7
Cleomar <cleomarcantil@gmail.com>
2021-04-05 13:16:03 -0300

    Criado endpoints para consulta de policies. Endpoint para verificação de permissões do usuário.

---
+++ WebApiSimplesCSharp/Constants/Policies/SecurityPolicies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiSimplesCSharp.HelpersExtensions.PolicyAuthorization;
+
+namespace WebApiSimplesCSharp.Constants.Policies
+{
+	[PolicyGroup(GROUP_NAME)]
+	[Description("Segurança")]
+	public static class SecurityPolicies
+	{
+		private const string GROUP_NAME = nameof(SecurityPolicies);
+		public static string GroupName => GROUP_NAME;
+
+		[Description("Consultar acessos")]
+		public const string ListarAcessos = GROUP_NAME + "." + nameof(ListarAcessos);
+
+		[Description("Consultar policies")]
+		public const string ListarPolicies = GROUP_NAME + "." + nameof(ListarPolicies);
+
+	}
+}
---
+++ WebApiSimplesCSharp/Controllers/SecurityController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApiSimplesCSharp.Constants.Policies;
+using WebApiSimplesCSharp.Services.Auth;
+using WebApiSimplesCSharp.Services.Permissoes;
+using static WebApiSimplesCSharp.HelpersExtensions.PolicyAuthorization.PolicyDiscover;
+
+namespace WebApiSimplesCSharp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class SecurityController : ControllerBase
+	{
+		private readonly IPermissaoCheckerService permissaoCheckerService;
+		private readonly IAuthService authService;
+		private readonly ILogger<SecurityController> logger;
+
+		public SecurityController(
+			IPermissaoCheckerService permissaoCheckerService,
+			IAuthService authService,
+			ILogger<SecurityController> logger)
+		{
+			this.permissaoCheckerService = permissaoCheckerService;
+			this.authService = authService;
+			this.logger = logger;
+		}
+
+		[Authorize(SecurityPolicies.ListarPolicies)]
+		[HttpGet("policies")]
+		public IEnumerable<PolicyGroupInfo> GetPolicies()
+		{
+			var allPolicyGroups = GetAllPolicyGroups();
+
+			return allPolicyGroups;
+		}
+
+		[Authorize(SecurityPolicies.ListarPolicies)]
+		[HttpGet("policies/{groupName}")]
+		public ActionResult<PolicyGroupInfo> GetPolicies(string groupName)
+		{
+			var groupPolicies = GetPolicyGroup(groupName);
+
+			if (groupPolicies is null) {
+				return NotFound($"PolicyGroup não encontrado: {groupName}");
+			}
+
+			return groupPolicies;
+		}
+
+		[AllowAnonymous]
+		[HttpGet("check-permissoes")]
+		public ExpandoObject CheckPermissoes([FromQuery(Name = "p")] string[] permissoes, [FromQuery] int? usuarioId = null)
+		{
+			usuarioId ??= authService.GetCurrentUserId();
+
+			var hasPermissoes = new ExpandoObject();
+
+			foreach (var p in permissoes) {
+				var hasPermissao = (usuarioId is not null) && permissaoCheckerService.HasPermissao(p, usuarioId.Value);
+				hasPermissoes.TryAdd(p, hasPermissao);
+			}
+
+			return hasPermissoes;
+		}
+
+	}
+}
